822a4505dfc47948c1ff2b4f37b8e147edae616d
Soheil Ghanbari <soheilghanbari1365@gmail.com>
2021-01-01 13:20:47 +0100

    Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from 'react'
 //Page Componnets
 import AboutSection from '../components/AboutSection'
+import ServicesSection from '../components/ServicesSection'
 
 const AboutUs = () => {
   return (
     <div>
       <AboutSection />
+      <ServicesSection />
     </div>
   )
 }
